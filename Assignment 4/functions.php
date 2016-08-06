777770c8198e2dd9e4b8cd6152083733e40dc075
hsingh13 <harvinderpalsingh@HarvindrpalsAir.home>
2016-08-06 13:13:38 -0400

    Assignment 4

---
+++ Assignment 4/functions.php
@@ -0,0 +1,45 @@
+<?php
+ 
+ 
+$Los_Angeles=new DateTime('now', new DateTimeZone('America/Los_Angeles'));
+
+
+$New_York=new DateTime('now', new DateTimeZone('America/New_York'));
+  
+$Sunrise="<img src='/Images/sunrise.png' />";
+$Sunset="<img src='/Images/sunset.jpg' />";
+
+
+if (date('a')=='am')
+{
+    echo "Sunrise in Los Angles .$Sunrise <br />";
+    echo "Sunrise in New York .$Sunrise <br />";
+} 
+If (date('a')=='pm')
+{
+     echo "Sunset in Los Angles .$Sunset <br />";
+     echo "Sunset in New York   .$Sunset <br />";
+}
+
+
+
+
+/*
+if ($Los_Angeles==date('F j, Y, g:i am'))
+{
+    echo "<div style=background-image:url('Images/sunrise.png');</div>";
+} else {
+   echo "<div style=background-image:url('Images/sunset.jpg');</div> ";
+}
+
+
+if ($New_York==date('F j, Y, g:i am'))
+{
+// echo '<img src="/Images/sunrise.png" alt="sunrise"/><br> ';
+echo "<div style=background-image:url('Images/sunrise.png');</div>";
+
+} else {
+   echo "<div style=background-image:url('Images/sunset.jpg');</div> ";
+} */
+
+?>

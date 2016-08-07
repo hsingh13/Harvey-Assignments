9fa1614879922ffdcbb987471d6351228727a8a8
hsingh13 <harvinderpalsingh@HarvindrpalsAir.home>
2016-08-06 22:52:20 -0400

    New Assignment

--- Assignment 4/index.php
+++ Assignment 4/index.php
@@ -4,14 +4,7 @@
 include 'header.php';
 require 'functions.php';
 
-//include (/HS-Assignment 3/Header);
-//include (/HS-Assignment 3/Footer);
-//include (/HS-Assignment 3/Functions);
 
-echo 'Los Angeles ', $Los_Angeles->format('F j, Y, g:i a');
-
-
-echo '<br> New York ', $New_York->format('F j, Y, g:i a');
 
 
 
